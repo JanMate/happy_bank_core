cdbc5d49aec60742ea3df81ef6724e75e1402467
Jan Matejka <jan.matejka142@gmail.com>
2021-11-24 18:07:11 +0100

    minor: Add bash exam file
    It adds bash_exam.sh file.
    
    Issue: no

---
+++ bash_exam.sh
@@ -0,0 +1,49 @@
+# Set/Select this file as a bash/shell script - you must set a header of the file
+# TODO
+
+# Add announcement that this script has just started with timestamp
+# TODO
+
+# Create a new "working_dir" variable and set it as a current directory
+#TODO
+
+# In your working dir, create nested directory structure "src/main/module" (discover the -p argument in mkdir command)
+# TODO
+
+# Create a new empty file ".gitkeep" in the "module" folder created in previous step
+# TODO
+
+# List the "module" directory mentioned in the previous step and the result store to a new variable
+# TODO
+
+# Check if the "module" directory is empty. If so or not, show a meaningful message about it
+# TODO
+
+# In your working directory create a new empty file "setup.ini"
+# TODO
+
+# You noticed you had wanted to create the "setup.ini" file to "module" folder, but you placed it to the working directory.
+# So move the file there
+# TODO
+
+# Your teammate warned you that the "src/main" is not standardized path and he asked you to remove it.
+# The content must be saved, and placed in the working directory
+# TODO
+
+# Fill "setup.ini" file with "user=admin" value
+# TODO
+
+# Print the content of the file from previous step for check and add a message about the file name before (A user always wants to be informed what's happening)
+# TODO
+
+# Count a number of the lines of the file mentioned in previous step and verify if it's equal to 1. If not, show a warning to user.
+# TODO
+
+# Show a message about the script's processing and sleep for 5 seconds
+# TODO
+
+# Remove "module" folder with its content and inform a user about it
+# TODO
+
+# Add announcement that this script has just finished with timestamp
+# TODO
