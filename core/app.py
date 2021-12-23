3a38755d04db21eea5581d2e228e984976a8e371
Jan Matějka <jan.matejka142@gmail.com>
2021-12-23 01:38:13 +0100

    feat: add rest api of core app
    Adds flask rest api definition into app.py file.
    
    Closes #6

--- core/app.py
+++ core/app.py
@@ -1,1 +1,25 @@
-print("Hello world")
+"""
+Runs happy bank core app.
+
+Represents REST Api layer.
+"""
+
+from flask import Flask
+
+api = Flask(__name__)
+
+
+@api.route('/health')
+def health():
+    """Returns health status"""
+    return 'Happy Bank Core app is up and running.', 200
+
+
+@api.route('/transfer/<sender>/<receiver>/<amount>')
+def transfer(sender, receiver, amount):
+    """Ensures transfer between 2 accounts of given money"""
+    return f'{sender} -> {receiver} ({amount})', 200
+
+
+if __name__ == '__main__':
+    api.run(debug=True, host='0.0.0.0')
