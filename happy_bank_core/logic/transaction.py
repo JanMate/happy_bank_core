7f91d7252240ed23b298a7f4bb5e075a690d1d25
Oleksandr6676 <oleksandr6676@gmail.com>
2022-02-15 15:21:23 +0100

    fix: minor fixes and improvements
    adds TransactionException class, small fixes and code improvements
    
    Closes #2

--- happy_bank_core/logic/transaction.py
+++ happy_bank_core/logic/transaction.py
@@ -1,46 +1,33 @@
 from account import Account
-from account import accounts
 
-a1 = Account("#1234")
-a2 = Account("#12345")
+
+class TransactionException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
 
 
 class Transaction:
     @staticmethod
-    def check_balance(customer, amount):
+    def check_balance(customer: Account, amount: float) -> bool:
         """Verifies if customer has enough money to transfer"""
-        amount = float(amount)
         balance = customer.deposit
-        if 0 < amount <= balance:
-            return True
-        else:
-            return False
+        return 0 < amount <= balance
 
     @staticmethod
-    def transfer(sender, recipient, amount):
+    def transfer(sender: Account, recipient: Account, amount: float) -> tuple | Exception:
         """Ensures transfer between 2 accounts"""
         if sender.id == recipient.id:
-            print(
-                "Accounts are equal, please enter correct account ID\n"
-                "----------------------------------------------------"
-            )
+            raise TransactionException("Sender and recipient ids should have different values")
         elif not Transaction.check_balance(sender, amount):
-            print(
-                "insufficient funds or incorrect amount was entered, "
-                "please try again\n"
-                "----------------------------------------------------"
-            )
-        else:
-            sender.deposit = round((sender.deposit - amount), 2)
-            recipient.deposit = round((recipient.deposit + amount), 2)
+            raise TransactionException("Insufficient balance or transfer amount <= 0")
 
-            accounts[sender.id]["deposit"] = sender.deposit
-            accounts[recipient.id]["deposit"] = recipient.deposit
-
-            print(
-                f"Following amount {amount} has been transferred "
-                f"from account {sender.id} to account {recipient.id}\n"
-                f"Current {sender.id} balance: {sender.deposit};\n"
-                f"Current {recipient.id} balance: {recipient.deposit}\n"
-                f"---------------------------------------------------"
-            )
+        sender.deposit = round((sender.deposit - amount), 2)
+        recipient.deposit = round((recipient.deposit + amount), 2)
+        print(
+            f"Following amount {amount} has been transferred "
+            f"from account {sender.id} to account {recipient.id}\n"
+            f"Current {sender.id} balance: {sender.deposit}\n"
+            f"Current {recipient.id} balance: {recipient.deposit}\n"
+            f"---------------------------------------------------"
+        )
+        return sender, recipient
