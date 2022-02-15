5c9f0acc339d3d7e1def02bb89cb79383b393a55
Oleksandr6676 <oleksandr6676@gmail.com>
2022-02-15 15:21:23 +0100

    fix: minor fixes and improvements
    adds TransactionException class, small fixes and code improvements
    
    Closes #2

--- happy_bank_core/logic/account.py
+++ happy_bank_core/logic/account.py
@@ -1,14 +1,9 @@
-accounts = dict()
-accounts["#1234"] = {"name": "John Doe", "deposit": 1000}
-accounts["#12345"] = {"name": "Johanna Doe", "deposit": 2000}
-
-
 class Account:
-    def __init__(self, customer):
-        """Gets customer's data from accounts dictionary"""
-        self.id = customer
-        self.name = accounts[self.id]["name"]
-        self.deposit = float(accounts[self.id]["deposit"])
+    def __init__(self, customer_id: str, full_name: str, balance: float):
+        """Gets customer's data"""
+        self.id = customer_id
+        self.name = full_name
+        self.deposit = balance
 
     def __str__(self):
         return f"Customer ID: {self.id};\n Name: {self.name};\n " f"Balance: {self.deposit}"
