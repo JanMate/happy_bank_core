a75544aab5fdefa4745cee2c068f1dc24da92a44
Oleksandr6676 <36409046+Oleksandr6676@users.noreply.github.com>
2022-02-22 16:22:47 +0100

    feat: add logger and exception handlers
    it adds logger calls and exception handlers
    
    Closes #36, #10

--- happy_bank_core/logic/transaction.py
+++ happy_bank_core/logic/transaction.py
@@ -1,4 +1,9 @@
 from .account import Account
+import logging
+import logging.config
+
+logging.config.fileConfig("../logging.conf")
+logger = logging.getLogger("root")
 
 
 class TransactionException(Exception):
@@ -11,23 +16,26 @@ class Transaction:
     def check_balance(customer: Account, amount: float) -> bool:
         """Verifies if customer has enough money to transfer"""
         balance = customer.deposit
+        logger.info(f"check_balance function return value: {0 < amount <= balance}")
         return 0 < amount <= balance
 
     @staticmethod
     def transfer(sender: Account, recipient: Account, amount: float) -> tuple:
         """Ensures transfer between 2 accounts"""
         if sender.id == recipient.id:
+            logger.error("Sender and recipient ids should have different values")
             raise TransactionException("Sender and recipient ids should have different values")
         elif not Transaction.check_balance(sender, amount):
+            logger.error("Insufficient balance or transfer amount <= 0")
             raise TransactionException("Insufficient balance or transfer amount <= 0")
 
         sender.deposit = round((sender.deposit - amount), 2)
         recipient.deposit = round((recipient.deposit + amount), 2)
-        print(
+        confirmation_message = (
             f"Following amount {amount} has been transferred "
-            f"from account {sender.id} to account {recipient.id}\n"
-            f"Current {sender.id} balance: {sender.deposit}\n"
-            f"Current {recipient.id} balance: {recipient.deposit}\n"
-            f"---------------------------------------------------"
+            f"from account {sender.id} to account {recipient.id}; "
+            f"Current {sender.id} balance: {sender.deposit}; "
+            f"Current {recipient.id} balance: {recipient.deposit};"
         )
-        return sender, recipient
+        logger.info(confirmation_message)
+        return sender, recipient, confirmation_message
