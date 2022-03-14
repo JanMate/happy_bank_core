0be1da53ce4ca98d267536dd71f1e5196e7cd1fd
Oleksandr6676 <36409046+Oleksandr6676@users.noreply.github.com>
2022-03-14 10:14:51 +0100

    fix: fix unittests for file/memory connectors
    implements fixes and improvements for file/memory connector unittests
    
    Closes #9

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -12,25 +12,45 @@ class TestMemory:
         }
         self.memory = MemoryConnector()
 
-    def test_if_read_function_returns_account_object(self):
+    def test_if_read_function_returns_account_instance(self):
+        """Tests if read function returns account instance"""
 
         # When
         result = self.memory.read("id321")
 
-        # Assert
+        # Then
         assert isinstance(result, Account)
 
-    def test_thrown_exception_if_account_not_found(self):
+    def test_if_read_function_returns_account_with_correct_id(self):
+        """Tests if read function returns account with correct ID"""
 
-        # Assert
+        # Given
+        expected_id = "id321"
+
+        # When
+        result = self.memory.read(expected_id)
+
+        # Then
+        assert expected_id == result.id
+
+    def test_if_read_function_throws_account_not_found_exception(self):
+        """Tests if read function throws AccountNotFound exception"""
+
+        # When
+        account_id = "does not exist"
+
+        # Then
         with pytest.raises(AccountNotFoundException):
-            self.memory.read("non_existing_id")
+            self.memory.read(account_id)
 
     def test_read_func_returns_updated_account(self):
+        """Tests if update function returns updated data"""
 
         # Given
         updated_account = Account("id321", "John Doe", 50)
+
+        # When
         result = self.memory.update(updated_account)
 
-        # Assert
+        # Then
         assert self.accounts != result
