34c1c562989458a236e065b1a0cc03f1f5744fb3
Oleksandr6676 <36409046+Oleksandr6676@users.noreply.github.com>
2022-03-14 10:14:51 +0100

    fix: fix unittests for file/memory connectors
    implements fixes and improvements for file/memory connector unittests
    
    Closes #9

--- tests/test_file_connector.py
+++ tests/test_file_connector.py
@@ -1,37 +1,71 @@
-import json
-
 from happy_bank_core.data.file_connector import FileConnector
 from happy_bank_core.logic.account import Account
 import pytest
-from unittest import mock
+from unittest.mock import patch, mock_open
+
+READ_TEST_DATA = (
+    '{"101": {"id": "101", "name": "John Doe", "deposit": 1000}, '
+    '"102": {"id": "102", "name": "Johanna Doe", "deposit": 1000}}'
+)
 
 
 class TestFileConnector:
-    def setup_method(self):
-        self.read_data = (
-            '{"101": {"id": "101", "name": "John Doe", "deposit": 1000},'
-            ' "102": {"id": "102", "name": "Johanna Doe", "deposit": 1000}}'
-        )
-        self.mock_data = mock.mock_open(read_data=self.read_data)
+    @patch("builtins.open", new_callable=mock_open, read_data=READ_TEST_DATA)
+    def test_if_read_function_returns_account_instance(self, mock_read):
+        """Tests if read function returns account instance"""
 
-    def test_if_read_function_returns_account_object(self):
-        with mock.patch("builtins.open", self.mock_data):
-            result = FileConnector.read("102")
-            assert isinstance(result, Account)
+        # When
+        result = FileConnector.read("102")
 
-    def test_if_read_function_throws_key_error(self):
-        with mock.patch("builtins.open", self.mock_data):
-            with pytest.raises(KeyError):
-                FileConnector.read("103")
+        # Then
+        mock_read.assert_called_once()
+        assert isinstance(result, Account)
 
-    def test_if_read_func_throws_file_not_found_error(self):
-        with mock.patch("builtins.open") as mocked_open:
-            mocked_open.side_effect = FileNotFoundError()
-            result = FileConnector.read("103")
-            assert not result
+    @patch("builtins.open", new_callable=mock_open, read_data=READ_TEST_DATA)
+    def test_if_read_func_return_account_with_correct_id(self, mock_read):
+        """Tests if read function returns account with correct ID"""
 
-    def test_if_update_func_returns_updated_values(self):
-        with mock.patch("builtins.open", self.mock_data):
-            account = Account("101", "John Doe", 2000)
-            result = FileConnector.update(account)
-            assert result != self.read_data
+        # Given
+        expected_id = "102"
+
+        # When
+        result = FileConnector.read(expected_id)
+
+        # Then
+        mock_read.assert_called_once()
+        assert expected_id == result.id
+
+    @patch("builtins.open", new_callable=mock_open, read_data=READ_TEST_DATA)
+    def test_if_read_function_throws_key_error(self, mock_read):
+        """Tests if read function throws KeyError exception"""
+
+        # Then
+        with pytest.raises(KeyError):
+            FileConnector.read("103")
+        mock_read.assert_called_once()
+
+    @patch("builtins.open", new_callable=mock_open, read_data=READ_TEST_DATA)
+    def test_if_read_func_throws_file_not_found_error(self, mock_read):
+        """Tests if read function throws FileNotFound exception"""
+
+        # When
+        mock_read.side_effect = FileNotFoundError()
+        result = FileConnector.read("103")
+
+        # Then
+        mock_read.assert_called_once()
+        assert not result
+
+    @patch("builtins.open", new_callable=mock_open, read_data=READ_TEST_DATA)
+    def test_if_update_func_returns_updated_values(self, mock_read):
+        """Tests if update function returns updated data"""
+
+        # Given
+        account = Account("101", "John Doe", 2000)
+
+        # When
+        result = FileConnector.update(account)
+
+        # Then
+        assert result != READ_TEST_DATA
+        mock_read.assert_called()
