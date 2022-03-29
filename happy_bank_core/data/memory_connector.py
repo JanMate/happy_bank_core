77d822546f023038b990a74adc03f925bb850f32
Oleksandr6676 <36409046+Oleksandr6676@users.noreply.github.com>
2022-03-29 09:01:05 +0200

    fix: fix connectors
    implements requested changes and improvements to connector classes
    
    Closes #4

--- happy_bank_core/data/memory_connector.py
+++ happy_bank_core/data/memory_connector.py
@@ -17,20 +17,20 @@ class MemoryConnector(Connector):
             "id456": Account("id456", "Johanna Doe", 1000),
         }
 
-    def read(self, account_id):
+    def read(self, account_id: str) -> Account:
         """Returns account with specific account id"""
         try:
             logger.info(self.accounts[account_id])
             return self.accounts[account_id]
         except KeyError as err:
-            logger.error(err)
+            logger.error(f"Account with id: {account_id} not found.")
             raise err
 
-    def update(self, account):
+    def update(self, account: Account):
         """Updates an existing account"""
         try:
             self.accounts[account.id] = account
             logger.info(self.accounts[account.id])
         except KeyError as err:
-            logger.error(err)
+            logger.error(f"Account with id: {account.id} not found.")
             raise err
